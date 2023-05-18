457a6872932c0f1d4ebcf1fee42efc8d873ffd28
Thiago <thiago.caitanov@gmail.com>
2023-05-17 22:37:29 -0300

    main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,6 @@ const limit= 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => console.log(jsonBody))
+    .catch((error) => console.error(error))
